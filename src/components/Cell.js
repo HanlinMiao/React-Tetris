e5d30097c7d206e63c7580e4acf7c60f64973e63
HanlinMiao <hanlin980505@gmail.com>
2025-04-26 20:26:18 -0400

    usePlayer and useStage hooks

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -3,7 +3,7 @@ import { StyledCell } from './styles/StyledCell';
 import { TETROMINOS } from '../tetrominos';
 
 const Cell = ({ type }) => (
-    <StyledCell type={"L"} color={TETROMINOS["L"].color}></StyledCell>
+    <StyledCell type={0} color={TETROMINOS[0].color}></StyledCell>
 )
 
 export default Cell;
